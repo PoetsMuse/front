8ef67e3b58899ed3034ac1e96eef42cccbd321c2
Kate Glushkova <weirdis1@gmail.com>
2023-11-20 20:01:25 +0200

    decorator added

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import reportWebVitals from './reportWebVitals';
-import { SnakeHead } from './snake/ui';
+import { SnakeHead, SnakeTail } from './snake/ui';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <SnakeHead />
+    <SnakeHead top={100} left={100} name="head" />
+    <SnakeTail top={200} left={200} name="tail" />
   </React.StrictMode>
 );
 
--- src/snake/ui.js
+++ src/snake/ui.js
@@ -1,12 +1,29 @@
+// import { useState } from 'react'
 import './snake.scss'
 
-export const SnakeHead = () => {
-
-    let direction = 'up'
-
+//HW2: using props.dir (destructuring and default value "up")
+//make it sot\ that the SnakeHead renders in either of the next 2 situations in index.js:
+//<SnakeHead />   or
+//<SnakeHead dir="right" />
+const Component = ({name}) => {
     return (
-        <div className={`snake__head dir-${direction}`}>
-            SnakeHead
-        </div>
+        <div className={name}></div>
     )
 }
+
+//decorator
+const withCoordinates = (Component) => {
+    return ({top, left, ...props}) => {
+        return (
+            <div style={{top: `${top}px`, left: `${left}px`, position: 'absolute'}}>
+                <Component {...props} />
+            </div>
+        )
+    }
+}
+
+const SnakeHead = withCoordinates(Component)
+const SnakeTail = withCoordinates(Component)
+
+
+export {SnakeHead, SnakeTail}
